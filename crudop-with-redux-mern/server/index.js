9d67db8059f9ef10323e406d44bb228528a9b7fc
Suresh Suthar <158549605+suthar-suresh@users.noreply.github.com>
2024-12-20 11:11:02 +0000

    added cors

--- crudop-with-redux-mern/server/index.js
+++ crudop-with-redux-mern/server/index.js
@@ -8,7 +8,7 @@ import router from "./routes/studentRoutes.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: 'https://fuzzy-parakeet-v66xqjjprg7whw66p-5173.app.github.dev/', // Replace with your frontend URL
+  origin: 'https://crud-op-with-redux-in-mern-v9t3.vercel.app/', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   credentials: true, // Allow cookies if needed
